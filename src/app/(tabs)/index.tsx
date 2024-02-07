ff47512a42e5c861e5ec698db2a4ee827afe0b31
hassiekho <haskondwani@gmail.com>
2024-02-07 12:55:44 +0200

    Changed folder structure

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+  
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
