ff3e83e9eee528445d9382e2957bccb33a341e39
hassiekho <haskondwani@gmail.com>
2024-02-08 11:10:08 +0200

    Added button component

--- src/app/(tabs)/menu/[productId].tsx
+++ src/app/(tabs)/menu/[productId].tsx
@@ -1,13 +1,18 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import products from '@assets/data/products'
+import Button from '@/components/Button'
 
 const sizes = ["S", "M", "L", "XL"]
 
-export default function product() {
-  const { id } = useLocalSearchParams()
-  const product = products.find(p => p.id.toString() === id)
+export default function ProductDetails() {
+  const [selectedSize, setSelectedSize] = useState("M")
+  const { productId } = useLocalSearchParams()
+  const product = products.find(p => p.id.toString() === productId)
+  const addToCart = () => {
+    console.warn("Product")
+  }
   if (!product) {
     return (
       <Text>Product not found</Text>
@@ -29,11 +34,20 @@ export default function product() {
       <Text>Select Size</Text>
       <View style={styles.sizes}>
         {sizes.map((size, index) => (
-          <View style={styles.size} key={index}>
-            <Text style={styles.sizeText}>{size}</Text>
-          </View>
+          <Pressable
+            onPress={() => { setSelectedSize(size) }}
+            style={[styles.size, {
+              backgroundColor: selectedSize === size ? "gainsboro" : "white"
+            }]}
+            key={index}>
+            <Text style={[styles.sizeText, {
+              color: selectedSize === size ? "black" : "gray"
+            }]}>{size}</Text>
+          </Pressable>
         ))}
       </View>
+      <Text className='text-base font-semibold'>${product.price}</Text>
+      <Button onPress={addToCart} text='Add to cart' />
     </View>
   )
 }
