2e429de20cc99a6ea66ba5e8af2e2fc57d269e5f
hassiekho <haskondwani@gmail.com>
2024-02-12 11:37:51 +0200

    Added Orders Screen

---
+++ src/app/(user)/orders/_layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+
+export default function MenuLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Orders",
+        }}
+      />
+    </Stack>
+  );
+}
